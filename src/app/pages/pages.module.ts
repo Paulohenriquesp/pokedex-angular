64d77a1569ba7b909c5308f369788cde6ccd79f1
Paulo <paulosouzap7@gmail.com>
2022-05-29 22:26:23 -0300

    criação component shared e component header

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoutingModule } from './routing.module';
+
+// Shared //
+import { SharedModule } from '../shared/shared.module';
+
+// Component //
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+
+
 
 
 
@@ -13,7 +20,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
